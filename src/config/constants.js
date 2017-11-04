5c8f4606e17ad03a973658923123169fd59e24d6
Filipe Prado <infra@klipbox.com.br>
2017-11-04 19:49:16 -0200

    Authorization with JWT token

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,11 +1,14 @@
 const devConfig = {
   MONGO_URL: 'mongodb://admin:trocalogo@ds249325.mlab.com:49325/nodeapi',
+  JWT_SECRET: 'thisisasecret',
 };
 const testConfig = {
   MONGO_URL: 'mongodb://admin:trocalogo@ds249325.mlab.com:49325/nodeapi',
+  JWT_SECRET: 'thisisasecret',
 };
 const prodConfig = {
   MONGO_URL: 'mongodb://admin:trocalogo@ds249325.mlab.com:49325/nodeapi',
+  JWT_SECRET: 'thisisasecret',
 };
 const defaultConfig = {
   PORT: process.env.PORT || 3000,
--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,7 +1,8 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
-
+import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
 import User from '../modules/users/user.model';
+import constants from '../config/constants';
 
 const localOpts = {
   usernameField: 'email',
@@ -10,11 +11,6 @@ const localOpts = {
 const localStrategy = new LocalStrategy(localOpts, async (email, password, done) => {
   try {
     const user = await User.findOne({ email });
-    console.log('====================================');
-    console.log(email, password);
-    console.log('====================================');
-    console.log(user);
-
     if (!user) {
       return done(null, false);
     } else if (!user.authenticateUser(password)) {
@@ -27,6 +23,27 @@ const localStrategy = new LocalStrategy(localOpts, async (email, password, done)
   }
 });
 
+// JWT Strategy
+const jwtOpts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken('Authorization'),
+  secretOrKey: constants.JWT_SECRET,
+};
+
+const jwtStrategy = new JWTStrategy(jwtOpts, async (payload, done) => {
+  try {
+    const user = await User.findById(payload._id);
+
+    if (!user) {
+      return done(null, false);
+    }
+    return done(null, user);
+  } catch (e) {
+    return done(e, false);
+  }
+});
+
 passport.use(localStrategy);
+passport.use(jwtStrategy);
 
 export const authLocal = passport.authenticate('local', { session: false });
+export const authJwt = passport.authenticate('jwt', { session: false });
