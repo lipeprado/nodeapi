b6267b5a95dde6679a2f8ba1739b355daf33046d
Filipe Prado <infra@klipbox.com.br>
2017-11-04 14:47:22 -0200

    Setup Mongose and Middlewares

--- dist/bundle.js
+++ dist/bundle.js
@@ -61,7 +61,7 @@ module.exports =
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -71,34 +71,197 @@ module.exports =
 "use strict";
 
 
-var _express = __webpack_require__(1);
-
-var _express2 = _interopRequireDefault(_express);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-const app = (0, _express2.default)();
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, err => {
-    if (err) {
-        throw err;
-    } else {
-        console.log(`Server running on port: ${PORT}
-        ------
-        Running on ${process.env.NODE_ENV}
-        ------
-        Time to Hack!
-        `);
-    }
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
+const devConfig = {
+  MONGO_URL: 'mongodb://admin:trocalogo@ds249325.mlab.com:49325/nodeapi'
+};
+const testConfig = {
+  MONGO_URL: 'mongodb://admin:trocalogo@ds249325.mlab.com:49325/nodeapi'
+};
+const prodConfig = {
+  MONGO_URL: 'mongodb://admin:trocalogo@ds249325.mlab.com:49325/nodeapi'
+};
+const defaultConfig = {
+  PORT: process.env.PORT || 3000
+};
+
+function envConfig(env) {
+  switch (env) {
+    case 'development':
+      return devConfig;
+    case 'test':
+      return testConfig;
+    default:
+      return prodConfig;
+  }
+}
+
+exports.default = Object.assign({}, defaultConfig, envConfig(process.env.NODE_ENV));
 
 /***/ }),
 /* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _express = __webpack_require__(2);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _constants = __webpack_require__(0);
+
+var _constants2 = _interopRequireDefault(_constants);
+
+__webpack_require__(3);
+
+var _middlewares = __webpack_require__(5);
+
+var _middlewares2 = _interopRequireDefault(_middlewares);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* eslint-disable no-console */
+
+const app = (0, _express2.default)();
+
+(0, _middlewares2.default)(app);
+
+app.get('/', (req, res) => {
+  res.send('Hello Node Api');
+});
+
+app.listen(_constants2.default.PORT, err => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`
+        --------------------------------------------------
+        Running on ${process.env.NODE_ENV}
+        Server running on port: ${_constants2.default.PORT}
+        Time to Hack!!  🌮 🐶 💪
+        --------------------------------------------------
+        `);
+  }
+});
+
+/***/ }),
+/* 2 */
 /***/ (function(module, exports) {
 
 module.exports = require("express");
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _mongoose = __webpack_require__(4);
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _constants = __webpack_require__(0);
+
+var _constants2 = _interopRequireDefault(_constants);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Remove the Warning with Promisse
+/* eslint-disable no-console */
+
+_mongoose2.default.Promise = global.Promise;
+
+// Connect the DB with URL
+
+try {
+  _mongoose2.default.connect(_constants2.default.MONGO_URL, {
+    useMongoClient: true
+    /* other options */
+  });
+} catch (err) {
+  _mongoose2.default.createConnection(_constants2.default.MONGO_URL);
+}
+
+_mongoose2.default.connection.once('open', () => console.log('mongoose DB running'));
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = require("mongoose");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _morgan = __webpack_require__(6);
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _bodyParser = __webpack_require__(7);
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _compression = __webpack_require__(8);
+
+var _compression2 = _interopRequireDefault(_compression);
+
+var _helmet = __webpack_require__(9);
+
+var _helmet2 = _interopRequireDefault(_helmet);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
+exports.default = app => {
+  if (isProd) {
+    app.use((0, _compression2.default)());
+    app.use((0, _helmet2.default)());
+  }
+  app.use(_bodyParser2.default.json());
+  app.use(_bodyParser2.default.urlencoded({ extended: true }));
+
+  if (isDev) {
+    app.use((0, _morgan2.default)('dev'));
+  }
+};
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("morgan");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+module.exports = require("body-parser");
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+module.exports = require("compression");
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+module.exports = require("helmet");
 
 /***/ })
 /******/ ]);
---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+
+import mongoose from 'mongoose';
+
+import constants from './constants';
+
+// Remove the Warning with Promisse
+mongoose.Promise = global.Promise;
+
+// Connect the DB with URL
+
+try {
+  mongoose.connect(constants.MONGO_URL, {
+    useMongoClient: true,
+    /* other options */
+  });
+} catch (err) {
+  mongoose.createConnection(constants.MONGO_URL);
+}
+
+mongoose.connection
+  .once('open', () => console.log('mongoose DB running'));
